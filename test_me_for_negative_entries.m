bea04afc8e0fe950c6f19088a97c8f4562e471bc
5y3datif <63924971+5y3datif@users.noreply.github.com>
2022-07-01 23:08:50 +0500

    Update test_me_for_negative_entries.m

--- test_me_for_negative_entries.m
+++ test_me_for_negative_entries.m
@@ -15,14 +15,29 @@ no_neg_h = ones(runs,1);
 % you may change the rank of factorization accordingly 
 k = 15;
 for ii=1: runs
+    % Uncomment the line number 19 and comment line number 21, if your machine has GPU.
+    % X = gpuArray(100*rand(m,n));
+    % Comment the line number 21, if your machine has GPU and you have already uncomment the line number 19.
     X = 100*rand(m,n);
     % Number of negative elements in X
-    no_neg_x(ii) = nnz(min(X,0));
-    % Uncomment the line number 5 and comment line number 7, if your machine has GPU.
-    % X = gpuArray(X);
-    % Comment the line number 7, if your machine has GPU and you have already uncomment the line number 5. 
+    no_neg_x(ii) = nnz(min(X,0))/(m*n);
     [W,H,Hhat,Y,Z,e,t,prpiter,tprp] = accNNSVD_PRP(X,k);
     % Number of negative elements in W and H
-    no_neg_w(ii) = nnz(min(W,0));
-    no_neg_h(ii) = nnz(min(H,0));
+    no_neg_w(ii) = nnz(min(W,0))/(m*k);
+    no_neg_h(ii) = nnz(min(H,0))/(k*n);
 end
+if ~isempty(find(no_neg_x,1,'first'))
+    fprintf("X in the %dth run contain negative entries.\n", find(no_neg_x,1,'first'));
+else
+    fprintf("X has no negative entries.\n");
+end
+
+if ~isempty(find(no_neg_w,1,'first'))
+    fprintf("W in the %dth run contain negative entries.\n", find(no_neg_w,1,'first'));
+else
+    fprintf("W has no negative entries.\n");
+end
+
+if ~isempty(find(no_neg_h,1,'first'))
+    fprintf("H in the %dth run contain negative entries.\n", find(no_neg_h,1,'first'));
+else
